d8db5d893be811412aa3a473c7202cd330ff6cb5
xiuqin <2023144348@qq.com>
2019-08-08 18:49:01 +0800

    111

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -12,8 +12,13 @@
         <form>
           <div :class="{on:loginWay}">
             <section class="login_message">
-              <input type="text" maxlength="11" placeholder="手机号" v-model="phone" 
-              v-validate="'required|mobile'" name="phone"
+              <input
+                type="tel"
+                maxlength="11"
+                placeholder="手机号"
+                v-model="phone"
+                v-validate="'required|mobile'"
+                name="phone"
               />
               <span style="color:red" v-show=" errors.has('phone')">{{ errors.first('phone') }}</span>
               <button
@@ -24,7 +29,15 @@
               >{{computeTime>0 ? `短信以发送(${computeTime}s)`: "获取验证码"}}</button>
             </section>
             <section class="login_verification">
-              <input type="tel" maxlength="8" placeholder="验证码" />
+              <input
+                type="text"
+                maxlength="8"
+                placeholder="验证码"
+                v-model="code"
+                v-validate="{ required: true, regex: /^\d{6}$/ }"
+                name="code"
+              />
+              <span style="color: red;">{{errors.first('code')}}</span>
             </section>
             <section class="login_hint">
               温馨提示：未注册硅谷外卖帐号的手机号，登录时将自动注册，且代表已同意
@@ -34,22 +47,56 @@
           <div :class="{on:!loginWay}">
             <section>
               <section class="login_message">
-                <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名" />
+                <input
+                  type="tel"
+                  maxlength="11"
+                  placeholder="用户名"
+                  v-model="name"
+                  v-validate="'required'"
+                  name="用户名"
+                />
+                <span style="color: red;">{{errors.first('用户名')}}</span>
               </section>
               <section class="login_verification">
-                <input :type="isShowPwd? 'text' : 'password'" maxlength="8" placeholder="密码" />
-                <div class="switch_button off" :class="isShowPwd? 'on' : 'off'"  @click.prevent="isShowPwd = !isShowPwd">
+                <input
+                  :type="isShowPwd? 'text' : 'password'"
+                  maxlength="8"
+                  placeholder="密码"
+                  v-model="pwd"
+                  v-validate="'required'"
+                  name="密码"
+                />
+                <span style="color: red;">{{errors.first('密码')}}</span>
+                <div
+                  class="switch_button off"
+                  :class="isShowPwd? 'on' : 'off'"
+                  @click.prevent="isShowPwd = !isShowPwd"
+                >
                   <div class="switch_circle" :class="{right:isShowPwd}"></div>
                   <span class="switch_text">{{isShowPwd ? 'abc' : ''}}</span>
                 </div>
               </section>
               <section class="login_message">
-                <input type="text" maxlength="11" placeholder="验证码" />
-                <img class="get_verification" src="./images/captcha.svg" alt="captcha" />
+                <input
+                  type="text"
+                  maxlength="11"
+                  placeholder="验证码"
+                  v-model="captcha"
+                  name="验证码"
+                  v-validate="{required: true, regex: /^.{4}$/}"
+                />
+                <img
+                  class="get_verification"
+                  ref="captcha"
+                  src="http://localhost:4000/captcha"
+                  @click="updateCaptcha"
+                  alt="captcha"
+                />
+                <span style="color: red;">{{errors.first('验证码')}}</span>
               </section>
             </section>
           </div>
-          <button class="login_submit">登录</button>
+          <button class="login_submit" @click="login">登录</button>
         </form>
         <a href="javascript:" class="about_us">关于我们</a>
       </div>
@@ -66,167 +113,246 @@ export default {
     return {
       loginWay: true, // true: 短信登陆, false: 密码登陆
       phone: "", // 手机号
+      code: "", //验证码
+      name: "", //用户名
+      pwd: "", //密码
+      captcha: "", // 图形验证码
       computeTime: 0, //计时剩余的时间
-      isShowPwd : false, //是否显示密码
-      mobile:'',
-    }
+      isShowPwd: false, //是否显示密码
+      mobile: ""
+    };
   },
   computed: {
     isRightPhone() {
-      return /^1\d{10}$/.test(this.phone)
+      return /^1\d{10}$/.test(this.phone);
     }
   },
   methods: {
     sendCode() {
-      this.computeTime = 10
-      const timer = setInterval(()=>{
-        this.computeTime--
-        if(this.computeTime===0){
-          clearInterval(timer)
+      this.computeTime = 10;
+      const timer = setInterval(() => {
+        this.computeTime--;
+        if (this.computeTime === 0) {
+          clearInterval(timer);
         }
-      },1000)
+      }, 1000);
+    },
+    //登录
+    async login() {
+      let names;
+      const { loginWay } = this;
+      if (loginWay) {
+        names = ["phone", "code"];
+      } else {
+        names = ["用户名", "密码", "验证码"];
+      }
+      const success = await this.$validator.validateAll(names); // 对指定的所有表单项进行验证
+      if (success) {
+        alert("表单验证通过, 发送登陆请求");
+      }
+    },
+    updateCaptcha() {
+      // 指定的src值需要携带一个时间戳参数
+      this.$refs.captcha.src =
+        "http://localhost:4000/captcha?time=" + Date.now();
     }
   }
-}
+};
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
-@import '../../common/stylus/mixins.styl'
-  .loginContainer
-    width 100%
-    height 100%
-    background #fff
-    .loginInner
-      padding-top 60px
-      width 80%
-      margin 0 auto
-      .login_header
-        .login_logo
-          font-size 40px
-          font-weight bold
-          color #02a774
-          text-align center
-        .login_header_title
-          padding-top 40px
-          text-align center
-          >a
-            color #333
-            font-size 14px
-            padding-bottom 4px
-            &:first-child
-              margin-right 40px
-            &.on
-              color #02a774
-              font-weight 700
-              border-bottom 2px solid #02a774
-      .login_content
-        >form
-          >div
-            display none
-            &.on
-              display block
-            input
-              width 100%
-              height 100%
-              padding-left 10px
-              box-sizing border-box
-              border 1px solid #ddd
-              border-radius 4px
-              outline 0
-              font 400 14px Arial
-              &:focus
-                border 1px solid #02a774
-            .login_message
-              position relative
-              margin-top 16px
-              height 48px
-              font-size 14px
-              background #fff
-              .get_verification
-                position absolute
-                top 50%
-                right 10px
-                transform translateY(-50%)
-                border 0
-                color #ccc
-                font-size 14px
-                background transparent
-                &.right_phone_number
-                  color:black
-            .login_verification
-              position relative
-              margin-top 16px
-              height 48px
-              font-size 14px
-              background #fff
-              .switch_button
-                font-size 12px
-                border 1px solid #ddd
-                border-radius 8px
-                transition background-color .3s,border-color .3s
-                padding 0 6px
-                width 30px
-                height 16px
-                line-height 16px
-                color #fff
-                position absolute
-                top 50%
-                right 10px
-                transform translateY(-50%)
-                &.off
-                  background #fff
-                  .switch_text
-                    float right
-                    color #ddd
-                &.on
-                  background #02a774
-                >.switch_circle
-                  //transform translateX(27px)
-                  position absolute
-                  top -1px
-                  left -1px
-                  width 16px
-                  height 16px
-                  border 1px solid #ddd
-                  border-radius 50%
-                  background #fff
-                  box-shadow 0 2px 4px 0 rgba(0,0,0,.1)
-                  transition transform .3s
-                  &.right
-                    transform translateX(27px)
-            .login_hint
-              margin-top 12px
-              color #999
-              font-size 14px
-              line-height 20px
-              >a
-                color #02a774
-          .login_submit
-            display block
-            width 100%
-            height 42px
-            margin-top 30px
-            border-radius 4px
-            background #4cd96f
-            color #fff
-            text-align center
-            font-size 16px
-            line-height 42px
-            border 0
-        .about_us
-          display block
-          font-size 12px
-          margin-top 20px
-          text-align center
-          color #999
-      .go_back
-        position absolute
-        top 5px
-        left 5px
-        width 30px
-        height 30px
-        >.iconfont
-          font-size 20px
-          color #999
+@import '../../common/stylus/mixins.styl';
 
+.loginContainer {
+  width: 100%;
+  height: 100%;
+  background: #fff;
+
+  .loginInner {
+    padding-top: 60px;
+    width: 80%;
+    margin: 0 auto;
+
+    .login_header {
+      .login_logo {
+        font-size: 40px;
+        font-weight: bold;
+        color: #02a774;
+        text-align: center;
+      }
+
+      .login_header_title {
+        padding-top: 40px;
+        text-align: center;
+
+        >a {
+          color: #333;
+          font-size: 14px;
+          padding-bottom: 4px;
+
+          &:first-child {
+            margin-right: 40px;
+          }
+
+          &.on {
+            color: #02a774;
+            font-weight: 700;
+            border-bottom: 2px solid #02a774;
+          }
+        }
+      }
+    }
+
+    .login_content {
+      >form {
+        >div {
+          display: none;
+
+          &.on {
+            display: block;
+          }
+
+          input {
+            width: 100%;
+            height: 100%;
+            padding-left: 10px;
+            box-sizing: border-box;
+            border: 1px solid #ddd;
+            border-radius: 4px;
+            outline: 0;
+            font: 400 14px Arial;
+
+            &:focus {
+              border: 1px solid #02a774;
+            }
+          }
+
+          .login_message {
+            position: relative;
+            margin-top: 16px;
+            height: 48px;
+            font-size: 14px;
+            background: #fff;
+
+            .get_verification {
+              position: absolute;
+              top: 50%;
+              right: 10px;
+              transform: translateY(-50%);
+              border: 0;
+              color: #ccc;
+              font-size: 14px;
+              background: transparent;
+
+              &.right_phone_number {
+                color: black;
+              }
+            }
+          }
+
+          .login_verification {
+            position: relative;
+            margin-top: 16px;
+            height: 48px;
+            font-size: 14px;
+            background: #fff;
+
+            .switch_button {
+              font-size: 12px;
+              border: 1px solid #ddd;
+              border-radius: 8px;
+              transition: background-color 0.3s, border-color 0.3s;
+              padding: 0 6px;
+              width: 30px;
+              height: 16px;
+              line-height: 16px;
+              color: #fff;
+              position: absolute;
+              top: 50%;
+              right: 10px;
+              transform: translateY(-50%);
+
+              &.off {
+                background: #fff;
+
+                .switch_text {
+                  float: right;
+                  color: #ddd;
+                }
+              }
+
+              &.on {
+                background: #02a774;
+              }
+
+              >.switch_circle {
+                // transform translateX(27px)
+                position: absolute;
+                top: -1px;
+                left: -1px;
+                width: 16px;
+                height: 16px;
+                border: 1px solid #ddd;
+                border-radius: 50%;
+                background: #fff;
+                box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+                transition: transform 0.3s;
+
+                &.right {
+                  transform: translateX(27px);
+                }
+              }
+            }
+          }
+
+          .login_hint {
+            margin-top: 12px;
+            color: #999;
+            font-size: 14px;
+            line-height: 20px;
+
+            >a {
+              color: #02a774;
+            }
+          }
+        }
+
+        .login_submit {
+          display: block;
+          width: 100%;
+          height: 42px;
+          margin-top: 30px;
+          border-radius: 4px;
+          background: #4cd96f;
+          color: #fff;
+          text-align: center;
+          font-size: 16px;
+          line-height: 42px;
+          border: 0;
+        }
+      }
+
+      .about_us {
+        display: block;
+        font-size: 12px;
+        margin-top: 20px;
+        text-align: center;
+        color: #999;
+      }
+    }
+
+    .go_back {
+      position: absolute;
+      top: 5px;
+      left: 5px;
+      width: 30px;
+      height: 30px;
+
+      >.iconfont {
+        font-size: 20px;
+        color: #999;
+      }
+    }
+  }
+}
 </style>
--- src/validate.js
+++ src/validate.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VeeValidate from 'vee-validate'
-import zh_CN from 'vee-validate/dist/locale/ar';
+import zh_CN from 'vee-validate/dist/locale/zh_CN';
 Vue.use(VeeValidate)
 
 //提示信息本地化
